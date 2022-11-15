16a702bc79e55820fbb9d9238d567fcc049062cf
WittySmirk <wyattdegenhart@gmail.com>
2022-11-14 19:55:10 -0700

    Really messy, but physics kind of sorta work

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use macroquad::prelude::*;
 // Constants for the game
 const SCALE: i32 = 3;
 const PLAYER_SIZE: Vec2 = Vec2::from_array([(13 * SCALE) as f32, (16 * SCALE) as f32]);
-const PLAYER_SPEED: f32 = 100f32;
 const PLAYER_FRAME_SPEED: f32 = 8f32;
 
 // Bunch of window settings
@@ -25,6 +24,14 @@ enum PlayerAnims {
     SwitchingDir,
 }
 
+enum PlayerState {
+    Idle,
+    Walking,
+    Jumping,
+    Running,
+    Skidding,
+}
+
 struct Player {
     hitbox: Rect,           // Basically our physics object that our texture projects to
     texture_map: Texture2D, // All the sprites for mario, later will be a texture atlas for the whole game
@@ -35,7 +42,12 @@ struct Player {
     x_move: f32,
     current_frame: f32,
     current_anim: PlayerAnims,
-    current_anim_mirrored: bool,
+    facing: bool, //false = right, true = left
+    running: bool,
+    velocity: Vec2,
+    state: PlayerState,
+    falling_accel: f32,
+    jumping: bool,
 }
 
 impl Player {
@@ -59,36 +71,23 @@ impl Player {
             frame_counter: 0f32,
             current_frame: 0f32,
             current_anim: PlayerAnims::Idle,
-            current_anim_mirrored: false,
+            state: PlayerState::Idle,
+            facing: false,
+            running: false,
+            velocity: Vec2::from_array([0f32, 0f32]),
             x_move: 0f32,
+            falling_accel: 0f32,
+            jumping: false,
         };
     }
 
     pub fn update(&mut self, dt: f32) {
-        //TODO: Create physics
-        //TODO: Convert Movement into vector
+        //TODO: Fix no deceleration bug
+        //TODO: Refactor physics (Enums)
 
-        // const MIN_WALK:    f32 = 4.453125f32;
-        // const MAX_WALK:    f32 = 93.75f32;
-        // const ACC_WALK:    f32 = 133.5975f32;
-        // const ACC_RUN:     f32 = 200.390625f32;
-        // const DEC_REL:     f32 = 182.8135f32;
-        // const DEC_SKID:    f32 = 365.625f32;
-        // const MIN_SKID:    f32 = 33.75f32;
-
-        // const STOP_FALL:   f32 = 1575f32;
-        // const WALK_FALL:   f32 = 1800f32;
-        // const RUN_FALL:    f32 = 2025f32;
-        // const STOP_FALL_A: f32 = 450f32;
-        // const WALK_FALL_A: f32 = 421.875f32;
-        // const RUN_FALL_A:  f32 = 562.5f32;
-
-        //Match tuple of moving left or right
-        let p_xmove: f32 = self.x_move;
-        match (
-            is_key_down(KeyCode::A) || is_key_down(KeyCode::Left),
-            is_key_down(KeyCode::D) || is_key_down(KeyCode::Right),
-        ) {
+        // Match tuple of moving left or right
+        // let p_xmove: f32 = self.x_move;
+        match (is_key_down(KeyCode::Left), is_key_down(KeyCode::Right)) {
             (true, false) => {
                 self.x_move = -1f32;
                 if let PlayerAnims::SwitchingDir = self.current_anim {
@@ -96,7 +95,7 @@ impl Player {
                     self.current_anim = PlayerAnims::Right;
                     // self.current_frame = 4f32;
                 }
-                self.current_anim_mirrored = true;
+                self.facing = true;
             }
             (false, true) => {
                 self.x_move = 1f32;
@@ -105,23 +104,165 @@ impl Player {
                     self.current_anim = PlayerAnims::Right;
                     // self.current_frame = 4f32;
                 }
-                self.current_anim_mirrored = false;
+                self.facing = false;
             }
-            _ => {
+            (false, false) => {
                 self.x_move = 0f32;
-                self.current_anim = PlayerAnims::Idle;
-                // self.current_frame = 0f32;
             }
+            _ => {}
         };
 
-        //Move based on direction
-        self.hitbox.x += self.x_move * dt * PLAYER_SPEED;
-
-        if ((p_xmove == -1f32 || p_xmove == 0f32) && self.x_move == 1f32)
-            || ((p_xmove == 1f32 || p_xmove == 0f32) && self.x_move == -1f32)
-        {
-            self.current_anim = PlayerAnims::SwitchingDir;
+        match is_key_down(KeyCode::X) {
+            true => {
+                self.running = true;
+            }
+            false => {
+                self.running = false;
+            }
         }
+
+        match is_key_down(KeyCode::Z) {
+            true => {
+                self.jumping = true;
+            }
+            false => {
+                self.jumping = false;
+            }
+        }
+
+        //Move based on direction
+        // self.hitbox.x += self.x_move * dt * PLAYER_SPEED;
+
+        // if ((p_xmove == -1f32 || p_xmove == 0f32) && self.x_move == 1f32)
+        // || ((p_xmove == 1f32 || p_xmove == 0f32) && self.x_move == -1f32)
+        // {
+        // self.current_anim = PlayerAnims::SwitchingDir;
+        // }
+        const MIN_WALK: f32 = 4.453125f32;
+        const MAX_WALK: f32 = 93.75f32;
+        const MAX_RUN: f32 = 153.75f32;
+        const ACC_WALK: f32 = 133.5975f32;
+        const ACC_RUN: f32 = 200.390625f32;
+        const DEC_REL: f32 = 182.8135f32;
+        const DEC_SKID: f32 = 365.625f32;
+        // const MIN_SKID: f32 = 33.75f32;
+
+        const STOP_FALL: f32 = 1575f32;
+        const WALK_FALL: f32 = 1800f32;
+        const RUN_FALL: f32 = 2025f32;
+        const STOP_FALL_A: f32 = 450f32;
+        const WALK_FALL_A: f32 = 421.875f32;
+        const RUN_FALL_A: f32 = 562.5f32;
+        const MAX_FALL: f32 = 270f32;
+
+        if let PlayerState::Jumping = self.state {
+            //Jumping
+            if self.velocity.x.abs() < MIN_WALK {
+                // slower than a walk // starting, stopping or turning around
+                self.velocity.x = 0f32;
+                self.state = PlayerState::Idle;
+                if self.x_move == -1f32 {
+                    self.velocity.x -= MIN_WALK;
+                }
+                if self.x_move == 1f32 {
+                    self.velocity.x += MIN_WALK;
+                }
+            } else if self.velocity.x.abs() >= MIN_WALK {
+                //Faster than a walk // accelerating or decelarating
+                if self.facing == false {
+                    if self.x_move == 1f32 {
+                        if self.running {
+                            self.velocity.x += ACC_RUN * dt;
+                        } else {
+                            self.velocity.x += ACC_WALK * dt;
+                        }
+                    } else if self.x_move == -1f32 {
+                        self.velocity.x += DEC_SKID * dt;
+                        self.state = PlayerState::Skidding;
+                    } else {
+                        self.velocity.x -= DEC_REL * dt;
+                    }
+                } else {
+                    if self.x_move == 1f32 {
+                        if self.running {
+                            self.velocity.x -= ACC_RUN * dt;
+                        }
+                    }
+                }
+            }
+
+            self.velocity.y += self.falling_accel * dt;
+
+            if self.jumping {
+                if self.velocity.x.abs() < 16f32 {
+                    self.velocity.y = -240f32;
+                    self.falling_accel = STOP_FALL;
+                } else if self.velocity.x.abs() < 40f32 {
+                    self.velocity.y = -240f32;
+                    self.falling_accel = WALK_FALL;
+                } else {
+                    self.velocity.y = -300f32;
+                    self.falling_accel = RUN_FALL;
+                }
+                self.state = PlayerState::Jumping;
+            }
+        } else {
+            //Not jumping
+            if self.velocity.y < 0f32 && self.jumping {
+                if self.falling_accel == STOP_FALL {
+                    self.velocity.y -= (STOP_FALL - STOP_FALL_A) * dt;
+                }
+                if self.falling_accel == WALK_FALL {
+                    self.velocity.y -= (WALK_FALL - WALK_FALL_A) * dt;
+                }
+                if self.falling_accel == RUN_FALL {
+                    self.velocity.y -= (RUN_FALL - RUN_FALL_A) * dt;
+                }
+            }
+
+            self.velocity.y += self.falling_accel * dt;
+
+            if self.x_move == 1f32 {
+                if self.velocity.x.abs() > MAX_WALK {
+                    self.velocity.x += ACC_RUN * dt;
+                } else {
+                    self.velocity.x += ACC_WALK * dt;
+                }
+            } else if self.x_move == -1f32 {
+                if self.velocity.x.abs() > MAX_WALK {
+                    self.velocity.x -= ACC_RUN * dt;
+                } else {
+                    self.velocity.x -= ACC_WALK * dt;
+                }
+            } else {
+                //Do nothing
+            }
+        }
+
+        //Cap max speeds
+        if self.velocity.y >= MAX_FALL {
+            self.velocity.y = MAX_FALL;
+        }
+        if self.velocity.y <= -MAX_FALL {
+            self.velocity.y = -MAX_FALL;
+        }
+
+        if self.velocity.x >= MAX_RUN {
+            self.velocity.x = MAX_RUN;
+        }
+        if self.velocity.x <= -MAX_RUN {
+            self.velocity.x = -MAX_RUN;
+        }
+        if self.velocity.x >= MAX_RUN && !self.running {
+            self.velocity.x = MAX_WALK;
+        }
+        if self.velocity.x <= -MAX_RUN && !self.running {
+            self.velocity.x = -MAX_WALK;
+        }
+
+        //Update position
+        self.hitbox.x += self.velocity.x * dt * (SCALE as f32);
+        self.hitbox.y += self.velocity.y * dt * (SCALE as f32);
 
         //Jump
         // if is_key_pressed(KeyCode::Space) || is_key_pressed(KeyCode::Z) {}
@@ -180,7 +321,7 @@ impl Player {
             DrawTextureParams {
                 dest_size: Some(PLAYER_SIZE),
                 source: Some(self.current_reference_frame),
-                flip_x: self.current_anim_mirrored,
+                flip_x: self.facing,
                 ..Default::default()
             },
         );
